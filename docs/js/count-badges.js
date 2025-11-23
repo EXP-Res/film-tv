400cc97e00bdaeaf46d043362d8809a6adb588b2
EXP <289065406@qq.com>
2025-11-23 20:13:12 +0800

    柯南

--- docs/js/count-badges.js
+++ docs/js/count-badges.js
@@ -15,8 +15,9 @@
         { file: 'section-05.html', index: 4 },
         { file: 'section-06.html', index: 5 },
         { file: 'section-07.html', index: 6 },
-        { file: 'section-98.html', index: 7 },
-        { file: 'section-99.html', index: 8 }
+        { file: 'section-08.html', index: 7 },
+        { file: 'section-98.html', index: 8 },
+        { file: 'section-99.html', index: 9 }
     ];
 
     /**
